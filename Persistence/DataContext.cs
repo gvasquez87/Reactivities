b776dc4389d7019a246240ee243e119c77378d8e
gvasquez87 <gavasmo87@gmail.com>
2021-08-11 12:11:39 -0600

    End Of Section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,22 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+
+        public DataContext( DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+
+
+    }
+}   
