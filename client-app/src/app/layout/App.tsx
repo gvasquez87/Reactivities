e5a2630276978234d97c9fe0bc9aeb29d38766b1
gvasquez87 <gavasmo87@gmail.com>
2021-08-19 14:24:13 -0600

    Cleaning

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
@@ -10,14 +8,13 @@ function App() {
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
       setActivities(response.data);
     });
   },[])
  
   return (
     <div>
-      <Header as='h2' icon='users' content='Ractivities'/>
+      <Header as='h2' icon='users' content='Reactivities'/>
         <List>
           {activities.map((activity: any) => (
               <List.Item key={activity.id}>
