70ad0fa99b6dc912114d4d3e619c4bbe675dafb8
gvasquez87 <gavasmo87@gmail.com>
2021-10-12 18:11:38 -0600

    Routing

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -41,3 +41,6 @@ const agent = {
 }
 
 export default agent;
+
+
+
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
 import { Button, Card,Image } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LodingComponent';
 import { useStore } from '../../../app/stores/store';
+import { Link } from 'react-router-dom';
 
 
-export default function ActivityDetails(){
+export default observer(function ActivityDetails(){
 
 
     const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const {selectedActivity: activity, loadActivity,loadingInitial} = activityStore;
+    //Nos traemos el Id de los params
+    const {id} = useParams<{id: string}>();
+
+    useEffect(() => {
+        if(id) loadActivity(id);
+    },[id,loadActivity])
+
+
+    if (loadingInitial || !activity) return <LoadingComponent />;
 
     if(!activity) return <LoadingComponent/>;
 
@@ -26,11 +39,11 @@ export default function ActivityDetails(){
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit'/>
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit'/>
+                    <Button as={Link} to='/activities' basic color='grey' content='Cancel'/>
                 </Button.Group>
             </Card.Content>
         </Card>
 
     )
-}
+})
