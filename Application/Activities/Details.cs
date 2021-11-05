45f278bf4486d980bba70f6c038c4f2f88cbfbdb
gvasquez87 <gavasmo87@gmail.com>
2021-11-05 15:34:58 -0600

    END of Section 10

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -4,19 +4,21 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Persistence;
+using Application.Core;
+
 
 namespace Application.Activities
 {
     public class Details
     {
 
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<Result<Activity>>
         {
             public Guid Id { get; set; }
 
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
             private readonly DataContext _context;
 
@@ -26,9 +28,10 @@ namespace Application.Activities
 
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities.FindAsync(request.Id);
+                return Result<Activity>.Success(activity);
             }
         }
 
